4955ec9fd0e8d80ecf7b0c89e24407ed4315c561
이호연 <ho991217@kakao.com>
2023-11-21 10:53:37 +0900

    .env delete

--- src/components/ui/navigation.tsx
+++ src/components/ui/navigation.tsx
@@ -106,21 +106,14 @@ export const NavigationItems: NavigationItem[] = [
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+          d="M20 21V19C20 17.9391 19.5786 16.9217 18.8284 16.1716C18.0783 15.4214 17.0609 15 16 15H8C6.93913 15 5.92172 15.4214 5.17157 16.1716C4.42143 16.9217 4 17.9391 4 19V21"
           stroke="white"
           strokeWidth="1.5"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
         <path
-          d="M12 16V12"
-          stroke="white"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M12 8H12.01"
+          d="M12 11C14.2091 11 16 9.20914 16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7C8 9.20914 9.79086 11 12 11Z"
           stroke="white"
           strokeWidth="1.5"
           strokeLinecap="round"
@@ -170,7 +163,7 @@ export default function Navigation() {
           <Link
             key={item.id}
             to={item.path}
-            className="flex flex-col items-center justify-center gap-1 px-4 text-white"
+            className="flex w-[25%] flex-col items-center justify-center gap-1 text-white"
           >
             {item.icon}
             <span className="text-xs font-light">{item.name}</span>
